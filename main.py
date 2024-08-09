c9cc2ee1f34fb6aaecfb716f40e1347a23721df2
Nix <nikusunix@gmail.com>
2024-08-09 22:14:30 +0300

    feat: add send price in telegram

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from telegram_notifications import send_notification_about_price
+
+load_dotenv()
+
+PRODUCT_NAME = 'Упаковка пива Чернігівське Титан 8% 1 л х 12 шт'
 
 
-
-d = {
-    'name': 'btnG',
-}
-
-d['name']
-
-
-def show_atribute(url,attr):
+def show_atribute(url, attr):
     r = requests.get(url)
     markup = r.text
-    soup = BeautifulSoup(markup,'html.parser')
-    teg = soup.find(attrs=attr)
-    return teg
+    soup = BeautifulSoup(markup, 'html.parser')
+    tag = soup.find(attrs=attr)
+    return tag
 
 
-print(f"""
-    Упаковка пива Чернігівське Титан 8% 1 л х 12 шт: {show_atribute("https://rozetka.com.ua/chernigivske-4820034925461/p362480604/",{"class":"product-price__big"}).text}
-    """)
+tag = show_atribute("https://rozetka.com.ua/chernigivske-4820034925461/p362480604/", {"class": "product-price__big"})
+send_status_code = send_notification_about_price(PRODUCT_NAME, tag.text)
 
+print(f"Упаковка пива Чернігівське Титан 8% 1 л х 12 шт: {tag.text}")
+print(f"Send to telegram status code: {send_status_code}")
