dc29c67c1b4be10168fdbbeed157e0c661376530
Maksim Polyshuk <polyshuk@gmail.com>
2024-08-09 21:44:51 +0300

    Реализовал функцию парсинга ценника с url

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.google.com.ua')
-markup = r.text
-soup = BeautifulSoup(markup,'html.parser')
-teg = soup.find(attrs={'name':"btnG"})
-print(teg.get("value"))
+
+
+d = {
+    'name': 'btnG',
+}
+
+d['name']
+
+
+def show_atribute(url,attr):
+    r = requests.get(url)
+    markup = r.text
+    soup = BeautifulSoup(markup,'html.parser')
+    teg = soup.find(attrs=attr)
+    return teg
+
+
+print(f"""
+    Упаковка пива Чернігівське Титан 8% 1 л х 12 шт: {show_atribute("https://rozetka.com.ua/chernigivske-4820034925461/p362480604/",{"class":"product-price__big"}).text}
+    """)
+
