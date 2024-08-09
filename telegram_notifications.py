b1956acebb6ecfaaa77bac84a318c49f7ed7b24d
Nix <nikusunix@gmail.com>
2024-08-09 22:53:30 +0300

    ref: add url arg for send in telegram

--- telegram_notifications.py
+++ telegram_notifications.py
@@ -5,8 +5,17 @@ DOTENV = dotenv_values(".env")
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{DOTENV['TELEGRAM_BOT_TOKEN']}/sendMessage"
 
 
-def send_notification_about_price(product, price):
-    response = requests.post(TELEGRAM_API_URL,
-                             data={'chat_id': DOTENV['TELEGRAM_CHAT_ID'], 'text': f'{product} - {price}'})
+def send_notification_about_price(product, price, url):
+    text = f"""
+            🛒 <b>Product Name:</b> {product}
+            💵 <b>Price:</b> {price}
+            🔗 <a href="{url}">Rozetka.ua</a>
+            """
+    request_data = {
+        'chat_id': DOTENV['TELEGRAM_CHAT_ID'],
+        'text': text,
+        'parse_mode': "HTML"
+    }
+    response = requests.post(TELEGRAM_API_URL, data=request_data)
     status_code = response.status_code
     return status_code
